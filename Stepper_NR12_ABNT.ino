776328f3b2fa4d046e97f6ae1b9025e4fd7c3f4a
martelima <martelima@gmail.com>
2019-02-25 10:13:18 -0300

    Create Stepper_NR12_ABNT.ino

---
+++ Stepper_NR12_ABNT.ino
@@ -0,0 +1,69 @@
+#define sirene 3
+int frequencia = 0;
+int tempo = 10;
+// Programa : Ponte H L298N e motor de passo
+// Autor : Arduino e Cia
+
+// Conexoes modulo - Arduino
+int IN1 = 8;
+int IN2 = 9;
+int IN3 = 10;
+int IN4 = 11;
+int leitura = 0;
+
+void setup()
+{
+  // Define os pinos como saida
+  Serial.begin(9600);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+}
+
+void loop()
+{
+  int leitura = analogRead(A0);
+  if (leitura > 300) {
+    // Passo 1
+    digitalWrite(IN1, 1);
+    digitalWrite(IN2, 0);
+    digitalWrite(IN3, 0);
+    digitalWrite(IN4, 1);
+    delay(tempo);
+
+    // Passo 2
+    digitalWrite(IN1, 0);
+    digitalWrite(IN2, 1);
+    digitalWrite(IN3, 0);
+    digitalWrite(IN4, 1);
+    delay(tempo);
+
+    // Passo 3
+    digitalWrite(IN1, 0);
+    digitalWrite(IN2, 1);
+    digitalWrite(IN3, 1);
+    digitalWrite(IN4, 0);
+    delay(tempo);
+
+    // Passo 4
+    digitalWrite(IN1, 1);
+    digitalWrite(IN2, 0);
+    digitalWrite(IN3, 1);
+    digitalWrite(IN4, 0);
+    delay(tempo);
+  } else {
+    for (frequencia = 150; frequencia < 1800; frequencia += 1) {
+      tone(sirene, frequencia, tempo);
+      delay(1);
+    }
+    for (frequencia = 1800; frequencia > 150; frequencia -= 1) {
+      tone(sirene, frequencia, tempo);
+      delay(1);
+      digitalWrite(IN1, 0);
+      digitalWrite(IN2, 0);
+      digitalWrite(IN3, 0);
+      digitalWrite(IN4, 0);
+    }
+  }
+}
